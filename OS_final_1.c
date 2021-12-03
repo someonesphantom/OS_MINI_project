8ab46561cd5c9e280fb981965004a180daf6f6c8
Autumn-1271 <60571326+Autumn-1271@users.noreply.github.com>
2021-12-03 22:25:44 +0530

    Update OS_final_1.c

--- OS_final_1.c
+++ OS_final_1.c
@@ -423,21 +423,25 @@ int main() {
 	
 	// int threadIndex = 0;	
 	int i,j,inp;
-	
+	do {
 		printf("Enter file_name:\n");
 		scanf("%s",file_name);
 		printf("Select the following:\n1:New\n2.Play\n3:exit\n");
 		scanf("%d",&inp);
 	
-		if(inp==1)
+		if(inp == 1)
 		{
 			ins();
 		}
-		if(inp==2)
+		if(inp == 2)
 		{
 			play();
 		}
+		if (inp == 3)
+		{
+			exit(0);
+		}
 		getsudoku();
 		checkSudo();
-	
+	} while(inp < 4);
 }
